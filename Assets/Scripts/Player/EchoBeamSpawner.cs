cefd939db7dfe2f9e11b34274ff205dccf32ba04
kasaxena <kasaxena@microsoft.com>
2023-05-20 15:15:39 +0530

    Added Echo beams

---
+++ Assets/Scripts/Player/EchoBeamSpawner.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EchoBeamSpawner : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField] EchoBeam echoBeamPrefab;
+
+    [Header("Player Configs")]
+    [SerializeField] private float duration;
+    [SerializeField] private float targetLength;
+
+    EchoBeam echoBeam;
+    [Header("Internal Configs")]
+    public bool spawnCompleted = false;
+    
+    public void InstantiateBeam()
+    {
+        spawnCompleted = false;
+        echoBeam =  Instantiate(echoBeamPrefab, transform.position, Quaternion.identity);
+        StartCoroutine(GrowEchoBeam(targetLength));
+    }
+
+
+    IEnumerator GrowEchoBeam(float targetValue)
+    {
+        float startValue = 0;
+        float value;
+        float time = 0;
+        Reset();
+
+        while (time < duration)
+        {
+            value = Mathf.Lerp(startValue, targetValue, time / duration);
+            Resize(value);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        _ = targetValue;
+        spawnCompleted = true;
+    }
+
+    public void Reset()
+    {
+        echoBeam.transform.position = transform.position; 
+        echoBeam.transform.localScale = new Vector3(0, echoBeam.transform.localScale.y, 0); 
+    }
+
+    public void Resize(float value)
+    {
+        Vector3 position = echoBeam.transform.position;
+        position.x = transform.position.x + value / 2;
+
+        echoBeam.transform.position = position;
+
+        Vector3 scale = echoBeam.transform.localScale;
+        scale.x = value;
+
+        echoBeam.transform.localScale = scale;
+    }
+}
